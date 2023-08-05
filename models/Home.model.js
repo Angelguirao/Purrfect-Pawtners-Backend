7fabe3c537c3cf6fd9959974c7fff8da5214fa84
Angel Guirao <angeguirao92@gmail.com>
2023-08-05 19:51:26 +0200

    added Home model

---
+++ models/Home.model.js
@@ -0,0 +1,32 @@
+const { Schema, model } = require("mongoose");
+
+
+const HomeSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    location: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    photo: {
+        type: String,
+        required: true
+    },
+    Owner: {
+        type: Schema.Types.ObjectId,
+        ref: 'User'
+    },
+    Friends: [{
+        type: Schema.Types.ObjectId,
+        ref: 'Cat'
+    }]
+});
+
+const Home = model('Home', HomeSchema);
+module.exports = Home;
