5216d56f86e47c33a8a70b02046b6fcd466954dc
Angel Guirao <angeguirao92@gmail.com>
2023-08-05 22:18:41 +0200

    added home routes and integrated into app.js but pending to add middlewares and test endpoints with POSTMAN or INSOMNIA

---
+++ routes/home.routes.js
@@ -0,0 +1,55 @@
+const router = require('express').Router()
+const Home = require('../models/Home');
+
+// POST request to create Home - "C"RUD
+router.post('/homes', async (req, res) => {
+    try {
+        const payload = req.body
+        const newHome = await Home.create(payload);
+        res.status(201).json(newHome);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+// GET request to get a list of Homes - C"R"UD
+router.get('/homes', async (req, res) => {
+    try {
+        const homes = await Home.find().populate('user').populate('cats');
+        res.status(200).json(homes);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+// GET request to get a specific Home - C"R"UD
+router.get('/homes/:id', async (req, res) => {
+    try {
+        const home = await Home.findById(req.params.id).populate('user').populate('cats');
+        res.status(200).json(home);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+// PUT request to update a Home - CR"U"D
+router.put('/homes/:id', async (req, res) => {
+    try {
+        const updatedHome = await Home.findByIdAndUpdate(req.params.id, req.body, {new: true});
+        res.status(202).json(updatedHome);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+// DELETE request to delete a Home - CRU"D"
+router.delete('/homes/:id', async (req, res) => {
+    try {
+        await Home.findByIdAndRemove(req.params.id);
+        res.status(202).json({message: "Home deleted"});
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+module.exports = router;
